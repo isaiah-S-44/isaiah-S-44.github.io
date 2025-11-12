dea0021694b17409fbc6efd04447d3b1690d7812
isaiah-S-44 <163367310+isaiah-S-44@users.noreply.github.com>
2025-11-11 16:05:14 -0800

    HW2: Add base structure and content
    HW 2 - US Geography Quiz

---
+++ cst336/hw/hw2_US_Geography_Quiz/js/script.js
@@ -0,0 +1,90 @@
+document.querySelector("button").addEventListener("click", gradeQuiz);
+
+//Global Variables
+var score = 0;
+var attempts = localStorage.getItem("attempts") ;
+
+displayQ4Choices();
+
+//Functions
+
+function displayQ4Choices(){
+    let q4ChoicesArray = ["Maine", "Rhode Island", "New Maryland", "Delaware"];
+    q4ChoicesArray = _.shuffle(q4ChoicesArray);
+    for (let i = 0; i < q4ChoicesArray.length; i++){
+        document.querySelector("#q4Choices").innerHTML += `<input type="radio" name="q4" id="${q4ChoicesArray[i]}" 
+            value="${q4ChoicesArray[i]}"> <label for="${q4ChoicesArray[i]}">${q4ChoicesArray[i]}</label>`;
+    }
+}//displayQ4Choices
+
+function isFormValid(){ 
+    let isValid = true;
+    if (document.querySelector("#q1").value == ""){
+        isValid = false;
+        document.querySelector("#validationFdbck").innerHTML = "Question 1 was not answered.";
+    }
+    return isValid;
+}//isFormValid
+
+function rightAnswer(index){
+    document.querySelector(`#q${index}Feedback`).innerHTML = "Correct!";
+    document.querySelector(`#q${index}Feedback`).className = "bg-success text-white!";
+    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/checkmark.png' alt='Checkmark'>";
+    score += 20;
+}
+function wrongAnswer(index){
+    document.querySelector(`#q${index}Feedback`).innerHTML = "Incorrect.";
+    document.querySelector(`#q${index}Feedback`).className = "bg-warning text-white!";
+    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/xmark.png' alt='xmark'>";
+}
+
+function gradeQuiz(){
+ console.log("Grading quiz…");
+ document.querySelector("#validationFdbck").innerHTML = ""; // resets validation feedback
+ if (!isFormValid()){
+    return; //stops the function if form is not valid
+ }
+
+ //variables
+ score = 0;
+ let q1Response = document.querySelector("#q1").value.toLowerCase();
+ let q2Response = document.querySelector("#q2").value;
+ let q4Response = document.querySelector("input[name='q4']:checked").value;
+//  console.log(q1Response);
+//  console.log(q2Response);
+ console.log(q4Response);
+
+
+ //Grading Question 1
+ if (q1Response === "sacramento"){
+    rightAnswer(1);
+ } else {
+    wrongAnswer(1);
+ }
+
+//Grading Question 2
+ if (q2Response === "mo"){
+    rightAnswer(2);
+ } else {
+    wrongAnswer(2);
+ }
+
+//Grading Question 3
+if (document.querySelector("#Jefferson").checked && document.querySelector("#Roosevelt").checked && 
+  !document.querySelector("#Jackson").checked && !document.querySelector("#Franklin").checked) {
+    rightAnswer(3);
+ } else {
+    wrongAnswer(3);
+ }
+
+//Grading Question 4
+ if (q4Response === "Rhode Island"){
+    rightAnswer(4);
+ } else {
+    wrongAnswer(4);
+ }
+    document.querySelector("#totalScore").innerHTML = `Total Score: ${score}`;
+    document.querySelector("#totalAttempts").innerHTML = `Total Attempts: ${++attempts}`;
+    localStorage.setItem("attempts", attempts);
+
+}//gradeQuiz
